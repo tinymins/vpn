3087017c62e2b0338715eb513eeab9455a595a31
Zhai YiMing <tinymins@gmail.com>
2015-04-21 13:24:30 +0800

    INIT

---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotRas;
+
+namespace VPN {
+class Program {
+    static void Main(string[] args) {
+        // default command is --start
+        if (args.Length == 0) args = new string[] { "--start" };
+
+        // try to get config file
+        Config cfg;
+        try {
+            cfg = new Config("vpn.bin");
+        } catch (Exception ex) {
+            Console.WriteLine("Open config file failed, please check if config file has been occupied by other application.");
+            return;
+        }
+
+        Console.Title = "VPN Tool";
+        // process command
+        switch (args[0]) {
+        /// help
+        case "-h":
+        case "--help":
+            Console.WriteLine("This application is write to connect vpns with the function of auto switch account.");
+            Console.WriteLine("source: https://github.com/tinymins/vpn");
+            Console.WriteLine("usage: vpn [--help] [<command>] [<args>]");
+            Console.WriteLine("");
+            Console.WriteLine("Commands:");
+            Console.WriteLine("  1. To config your vpn adapter name:");
+            Console.WriteLine("     vpn [-e|--set-entry] YOUR_VPN_CONNECTION_NAME");
+            Console.WriteLine("  2. To add or update password of vpn account list:");
+            Console.WriteLine("     vpn [-a|--add-account] HOSTNAME USERNAME PASSWORD");
+            Console.WriteLine("  3. To remove vpn account:");
+            Console.WriteLine("     vpn [-d|--delete-account] HOSTNAME USERNAME");
+            Console.WriteLine("  4. To list vpn account:");
+            Console.WriteLine("     vpn [-l|--list-account]");;
+            Console.WriteLine("  5. To connect vpn:");
+            Console.WriteLine("     vpn [-s|--start]");
+            Console.WriteLine("");
+            Console.WriteLine("Samples:");
+            Console.WriteLine("  vpn --set-entry MyVPN");
+            Console.WriteLine("  vpn --add-account vpn.sample.com root password");
+            Console.WriteLine("  vpn --start");
+            Console.WriteLine("");
+            Console.WriteLine("NOTICE: always remember to input enough arguments.");
+            break;
+        // set entry
+        case "-e":
+        case "--set-entry":
+            if (CheckArgument(args, 2)) {
+                cfg.EntryName = args[1];
+            }
+            break;
+        // add account record
+        case "-a":
+        case "--add-account":
+            if (CheckArgument(args, 4)) {
+                cfg.AddVPN(args[1], args[2], args[3]);
+            }
+            break;
+        // delete account record
+        case "-d":
+        case "--delete-account":
+            if (CheckArgument(args, 3)) {
+                cfg.DelVPN(args[1], args[2]);
+            }
+            break;
+        // list account record
+        case "-l":
+        case "--list-account":
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("{0,-30}{1,-16}", "Host", "Username");
+            Console.WriteLine("----------------------------------------------");
+            foreach (Config.VPN vpn in cfg.GetVPNList()) {
+                Console.WriteLine("{0,-30}{1,-16}", vpn.Hostname, vpn.Username);
+            }
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Entry Name: " + cfg.EntryName);
+            Console.WriteLine("----------------------------------------------");
+            break;
+        /// start checking connection status and auto connect
+        case "-s":
+        case "--start":
+            if (cfg.EntryName == "" || cfg.GetVPNList().Length == 0) {
+                Console.WriteLine("Please config your adapter and vpn account first!");
+                Console.WriteLine("Try command --help to see samples.");
+            } else {
+                Console.WriteLine("Start monitoring vpn connect successfully.");
+                VPNHelper vpn = new VPNHelper();
+                while (true) {
+                    bool bDisconnected = true;
+                    foreach (RasConnection conn in RasConnection.GetActiveConnections()) {
+                        if (conn.EntryName == cfg.EntryName) bDisconnected = false;
+                        // RasConnectionStatus status = conn.GetConnectionStatus();
+                        // Console.WriteLine(conn.EntryName);
+                        // Console.WriteLine(status.ConnectionState);
+                        // Do something useful.
+                    }
+                    if (bDisconnected) {
+                        foreach (Config.VPN rec in cfg.GetVPNList()) {
+                            Console.WriteLine("Connecting via " + cfg.EntryName + " to " + rec.Hostname + " with account " + rec.Username + "...");
+                            // check if entry is busy
+                            while (vpn.IsEntryBusy(cfg.EntryName)) {
+                                Console.WriteLine("Entry busy... Retry in 10 sec...");
+                                System.Threading.Thread.Sleep(10000);
+                            }
+                            // try to connect
+                            string msg = vpn.Connect(cfg.EntryName, rec.Hostname, rec.Username, rec.Password);
+                            if (msg == "") {
+                                Console.WriteLine("Connected...");
+                                break;
+                            } else {
+                                Console.WriteLine("Connect failed with information: " + msg );
+                            }
+                        }
+                    }
+                    System.Threading.Thread.Sleep(5000);
+                }
+            }
+            break;
+        default:
+            Console.WriteLine("Bad argument!");
+            Console.WriteLine("Try command --help to see samples.");
+            break;
+        }
+    }
+
+    private static bool CheckArgument(string[] args, int p) {
+        if (args.Length != p) {
+            Console.WriteLine("Wrong number of arguments for command '" + args[0] + "': " + (p - 1) + " expect, " + (args.Length - 1) + " received.");
+        }
+        return args.Length == p;
+    }
+}
+}
---
+++ VPNHelper.cs
@@ -0,0 +1,79 @@
+﻿using DotRas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPN {
+class VPNHelper {
+    private RasDialer Dialer;
+    private RasPhoneBook AllUsersPhoneBook;
+    public VPNHelper() {
+        this.Dialer = new RasDialer();
+        this.AllUsersPhoneBook = new RasPhoneBook();
+    }
+
+    /// <summary>
+    /// Connect vpn
+    /// </summary>
+    /// <param name="entryname"></param>
+    /// <param name="hostname"></param>
+    /// <param name="username"></param>
+    /// <param name="password"></param>
+    /// <returns></returns>
+    public string Connect(string entryname, string hostname, string username, string password) {
+        SetEntry(entryname, hostname);
+        this.Dialer.EntryName = entryname;
+        this.Dialer.PhoneBookPath = AllUsersPhoneBook.Path; //RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.AllUsers);
+        try {
+            // Set the credentials the dialer should use.
+            this.Dialer.Credentials = new NetworkCredential(username, password);
+
+            // NOTE: The entry MUST be in the phone book before the connection can be dialed.
+            // Begin dialing the connection; this will raise events from the dialer instance.
+            this.Dialer.Dial();
+        } catch (Exception ex) {
+            return ex.Message;
+        }
+        return "";
+    }
+
+    /// <summary>
+    /// Check if entry is busy now.
+    /// </summary>
+    /// <param name="entryname"></param>
+    /// <returns></returns>
+    public bool IsEntryBusy(string entryname) {
+        // this.Dialer.EntryName = entryname;
+        // this.Dialer.PhoneBookPath = AllUsersPhoneBook.Path;
+        return this.Dialer.IsBusy;
+    }
+
+    /// <summary>
+    /// create/update vpn entry
+    /// </summary>
+    /// <param name="name">name of the connection</param>
+    /// <param name="ip">ip address of connection</param>
+    public void SetEntry(string name, string ip) {
+        // This opens the phonebook so it can be used. Different overloads here will determine where the phonebook is opened/created.
+        this.AllUsersPhoneBook.Open(true);
+        // check if this entry has already exist
+        if (!this.AllUsersPhoneBook.Entries.Contains(name)) {
+            // create entry
+            if (ip == "") ip = IPAddress.Loopback.ToString();
+            // Create the entry that will be used by the dialer to dial the connection. Entries can be created manually, however the static methods on
+            // the RasEntry class shown below contain default information matching that what is set by Windows for each platform.
+            RasEntry entry = RasEntry.CreateVpnEntry(name, ip, RasVpnStrategy.PptpFirst,
+                             RasDevice.GetDeviceByName("(PPTP)", RasDeviceType.Vpn));
+            // Add the new entry to the phone book.
+            this.AllUsersPhoneBook.Entries.Add(entry);
+        } else if (ip != "") {
+            // update entry
+            this.AllUsersPhoneBook.Entries[name].PhoneNumber = ip;
+            this.AllUsersPhoneBook.Entries[name].Update();
+        }
+    }
+}
+}
